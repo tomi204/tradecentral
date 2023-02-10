4c3771f9d78395ac96cdbfa45342ea7a0d176dd7
tomas <tomi204@outlook.com>
2023-02-09 23:07:01 -0300

    test create user

---
+++ contracts/test/Trade.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import {TradeCentral} from "../src/NewTradeCentral.sol";
+
+contract testTradeCentral is Test {
+    TradeCentral public trade;
+    // create some test users addresses
+    address alex = address(0x1);
+    address tomi = address(0x2);
+    address abel = address(0x3);
+
+    function setUp() public {
+        trade = new TradeCentral();
+    }
+
+    // creates a test user with the address 0x1
+    function testCreateTrade() public {
+        // create user
+        string memory _email = "tomi@hotmail.com";
+        string memory _name = "Tomi";
+        string memory image = "https://i.imgur.com/1ZQZ1YR.png";
+        // get user address
+        address userAddress = vm.addr(uint256(uint160(tomi)));
+        vm.prank(userAddress);
+        trade.createUser(_email, _name, image);
+        // asset user lookup
+    }
+}
