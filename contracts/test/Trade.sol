d9e35d273a5156c16fee78b01e884bd3ed834daa
tomas <tomi204@outlook.com>
2023-02-10 23:38:09 -0300

    test with foundry

--- contracts/test/Trade.sol
+++ contracts/test/Trade.sol
@@ -10,21 +10,46 @@ contract testTradeCentral is Test {
     address alex = address(0x1);
     address tomi = address(0x2);
     address abel = address(0x3);
+    address userAddress = vm.addr(uint256(uint160(tomi)));
 
     function setUp() public {
         trade = new TradeCentral();
     }
 
     // creates a test user with the address 0x1
-    function testCreateTrade() public {
+    function testCreateUser() public {
         // create user
         string memory _email = "tomi@hotmail.com";
         string memory _name = "Tomi";
         string memory image = "https://i.imgur.com/1ZQZ1YR.png";
         // get user address
-        address userAddress = vm.addr(uint256(uint160(tomi)));
         vm.prank(userAddress);
         trade.createUser(_email, _name, image);
         // asset user lookup
+    }
+
+    function createTrade() public {
+       ///create trade
+       string memory _name = "Trade 1";
+       string memory _description = "Trade 1 description";
+       string memory _image = "https://i.imgur.com/1ZQZ1YR.png";
+       string memory _category = "TV/MONITORES";
+       string memory _location = "Argentina";
+
+         trade.createTrade(200,_name, _description, _image, _category, _location);
+    }
+
+    function cancelTrade() public{
+        // cancel trade
+        trade.cancelTrade(1);
+    }
+
+    function updateUserProfile() public{
+        vm.prank(tomi);
+         string memory _email = "tomi@hotmail.com";
+        string memory _name = "Tomi";
+        string memory image = "https://i.imgur.com/1ZQZ1YR.png";
+
+        trade.updateProfile(_email, _name, image);
     }
 }
